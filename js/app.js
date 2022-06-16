79d943b95ed4d43a41ba53ab3e6a3c2cfb7c8cb5
Brian Li <25bli@crschools.us>
2022-06-16 16:06:02 -0500

    7 and 8 progress

--- js/app.js
+++ js/app.js
@@ -13,23 +13,39 @@ function userName(){
 userName();
 
 
-
+function displayRating(){
+  let output = '';
+  let rating = prompt('Scale of 1 to 5, how many stars?');
+  
+  for(let i = 0;  i < rating; i++){
+    output += '<img class="Rroll" src="https://i.pinimg.com/originals/60/c1/4a/60c14a43fb4745795b3b358868517e79.png" />'
+  
+  }
+  
+   return document.write(output);
+  }
+  displayRating();
 
 
 function userShoeResponse(){
+  
+do {
   let shoeSelectionNum =  parseInt(prompt('Do you like shoes? press 1 for yes and 3 for no'));
-
-  console.log(typeof shoeSelectionNum);
-
-
-
+  //if they say 1
+  
   if(shoeSelectionNum === 1){
     message = 'Good! Your in the right place!';
-  } else if (shoeSelectionNum === 3){
-    message = 'Who doesnt like shoes???';
-  } else {
-    message = ' can you count? ';
+    break;
   }
+ else if (shoeSelectionNum === 3){
+  message = 'Who doesnt like shoes???';
+  break;
+} else {
+  alert('Try again!');
+}
+} while(true)
+
+
 
 }
 
