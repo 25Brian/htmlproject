a5008c347493e963f685928123e631c1961c116a
Brian Li <25bli@crschools.us>
2022-06-16 13:35:09 -0500

    javascript

--- js/app.js
+++ js/app.js
@@ -2,12 +2,11 @@
 
 console.log('function demo is connected')
 
-
 userName();
 
 function userName(){
-    let userName = prompt('what is your name ');
-    console.log ('username is', userName);
+    let userName = prompt('what is your name?');
+    console.log ('username', userName);
 }
 
 let shoe = prompt('Do you like shoes? press 1 for yes and 3 for no');
@@ -20,12 +19,12 @@ console.log('did it turn into a number?', shoeNumber);
 
 let message;
 
-if(shoe ==1){
+if(shoe ===1){
     message = 'Good! Your in the right place!';
-} else if (shoe ==3){
+} else if (shoe ===3){
     message = 'Who doesnt like shoes???';
 } else {
     message = ' can you count? ';
 }
  
-document.write('Hey ' + userNam + ' ! ' + message);
+document.write('Hey ' + userName + ' ! ' + message);
